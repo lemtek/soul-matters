1b6a650555681936300f7cf07faca63950f31799
Lee Bhogal <lemtekservice@gmail.com>
2016-01-13 22:59:13 +0000

    Update ESP_Main_Library.js

--- ESP_Main_Library.js
+++ ESP_Main_Library.js
@@ -7,19 +7,33 @@
 // v1.0 - 
 // ******************************************************************************* //
 
-function displayClientAlert_onSave() {
+// Used to display client alerts on the Quote entity
+    function quote_Display_Alerts() {
 
-    // get the value of the customerid field
-    lookupFieldObject = Xrm.Page.data.entity.attributes.get('customerid');
-    if (lookupFieldObject.getValue() != null) {
-        var entityId = lookupFieldObject.getValue()[0].id;
-        var entityName = lookupFieldObject.getValue()[0].entityType;
-        var entityLabel = lookupFieldObject.getValue()[0].name;
-    }
+        var alertArray = null;
+        var myAlert = null;
 
-        // set the value of the new_clientalerts field
+        // Specify the connection URL
+        var serverURL = Xrm.Page.context.getClientUrl();
+        var queryURL = serverURL + "/XRMServices/2011/OrganizationData.svc/new_alertsSet?$select=new_alertsId,new_Alert,new_Message,new_ClientId,new_Category"
+
+        // Sets the Html request
+        var req = new XMLHttpRequest();
+        req.open("GET", queryURL, false) // Must specify false here to run synchronously
+        req.setRequestHeader("Accept", "application/json");
+        req.setRequestHeader("Content-Type", "application/json; charset=utf-8");
+        req.send();
+
+        // Process the results
+        var results = JSON.parse(req.responseText).d.results;
+        if (results != null && results.length > 0) {
+            alertArray = results[0];
+            myAlert = alertArray.new_Alert;
+        }
+
+       // set the value of the new_clientalerts field
         Xrm.Page.ui.tabs.get("Summary_tab").sections.get("alerts_section").setVisible(true);
-        Xrm.Page.getAttribute("new_clientalerts").setValue(entityLabel);
-        Xrm.Page.getAttribute("new_clientalerts").setSubmitMode("always");
+        Xrm.Page.getAttribute("new_clientalerts").setValue(myAlert);
+        Xrm.Page.getAttribute("new_clientalerts").setSubmitMode("always"); // always put this after setting the value as a field to prevent readonly errors
 
-}
+    }
